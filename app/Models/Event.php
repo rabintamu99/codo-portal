eb15b6b78df94134acf147c66add5b1754f364e8
rabin <rabing59@gmail.com>
2024-01-29 22:33:03 +0900

    calendar updated

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = [
+        'user_id',
         'name',
         'start',
         'end',
--- database/migrations/2024_01_09_083029_create_events_table.php
+++ database/migrations/2024_01_09_083029_create_events_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->dateTime('start');
             $table->dateTime('end');
